46e9d42a247009245a25e3b84f68c4bc03e0114c
Ann <ann@Anns-MBP.attlocal.net>
2023-01-30 13:19:21 -0800

    footer sectino

--- components/common/FooterBottom/FooterBottom.js
+++ components/common/FooterBottom/FooterBottom.js
@@ -7,6 +7,9 @@ import styles from './FooterBottom.module.css'
 const FooterBottom = () => {
   return (
     <div className={styles.footerBottom}>
+      <div className={styles.logo} onClick={()=>router.push('/')}>
+        <img src={'/logos/community_logo.svg'} alt='community logo' />
+      </div>
       {/* <Logo color='multicolor' className={styles.logo} /> */}
       {/* <Copyright /> */}
     </div>
