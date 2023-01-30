c59e4a4b3aba9061184507396f81439c8088e6d8
Sharon Tulman <stulman1121@gmail.com>
2023-01-30 14:16:49 -0800

    fix: fixed router not defined error in footer

--- components/common/FooterBottom/FooterBottom.js
+++ components/common/FooterBottom/FooterBottom.js
@@ -3,8 +3,10 @@ import React from 'react'
 import Logo from '../Logo/Logo'
 import Copyright from '../Copyright/Copyright'
 import styles from './FooterBottom.module.css'
+import { useRouter } from 'next/router'
 
 const FooterBottom = () => {
+  const router = useRouter()
   return (
     <div className={styles.footerBottom}>
       <div className={styles.logo} onClick={()=>router.push('/')}>
