2d4bbce9382a3ea30ae83116ef2b4648de2c902d
Sharon Tulman <stulman1121@gmail.com>
2023-05-16 14:18:44 -0700

    fix: share buttons in header style

--- src/components/common/Layout/Header/NavLinks/HeaderSharebuttons.js
+++ src/components/common/Layout/Header/NavLinks/HeaderSharebuttons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../Header.module.css'
+import styles from './HeaderShareButtons.module.css'
 
 const HeaderShareButtons = ({ 
   postTitle = 'Help our community in winning broadband grants by taking this internet speed test'
@@ -7,13 +7,14 @@ const HeaderShareButtons = ({
   const testUrl = window.location.origin
   return (
     <div className={styles.headerShareButtons}>
-      <div className={styles.iconLinkTwitter}>
+      <div className={styles.button}>
         <a
           href={`https://twitter.com/share?url=${testUrl}&text=${postTitle}`}
           target='_blank'
           rel='noreferrer'
         >
           <img
+            className={styles.icon}
             src='/icons/twitter-2.svg'
             height={22}
             width={22}
@@ -21,28 +22,29 @@ const HeaderShareButtons = ({
           />
         </a>
       </div>
-      <div className={styles.iconLinkLinkedIn}>
+      <div className={styles.button}>
         <a
           href={`https://www.linkedin.com/sharing/share-offsite/?url=${testUrl}`}
           target='_blank'
           rel='noreferrer'
         >
-          <img
+          <img 
+            className={styles.icon}
             src='/icons/linkedin-2.svg'
             height={19}
             width={19}
-            alt='community toolkit linkedin link'
-            style={{ fill: 'red' }}
-          />
+            alt='community toolkit linkedin link'>
+            </img>
         </a>
       </div>
-      <div className={styles.iconLinkFacebook}>
+      <div className={styles.button}>
         <a
           href={`https://www.facebook.com/sharer.php?u=${testUrl}`}
           target='_blank'
           rel='noreferrer'
         >
           <img
+            className={styles.icon}
             src='/icons/facebook-2.svg'
             height={22}
             width={22}
@@ -50,13 +52,14 @@ const HeaderShareButtons = ({
           />
         </a>
       </div>
-      <div className={styles.iconLinkEmail}>
+      <div className={styles.button}>
         <a
           href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=&su=${postTitle}&body=${testUrl}+&ui=2&tf=1&pli=1`}
           target='_blank'
           rel='noreferrer'
         >
           <img
+            className={styles.icon}
             src='/icons/mail.svg'
             height={22}
             width={22}
