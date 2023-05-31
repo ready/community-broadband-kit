27c35853b5b6ff1e353603db5f7505ad7ebcef14
Sharon Tulman <stulman1121@gmail.com>
2023-05-31 13:36:58 -0700

    fix: info sections

--- src/components/Home/WhyTakeTest/WhyTakeTest.js
+++ src/components/Home/WhyTakeTest/WhyTakeTest.js
@@ -38,6 +38,7 @@ const WhyTakeTest = ({ page }) => {
 
   return (
     <section className={styles.reportSection} style={{ backgroundColor: steps?.backgroundColor }}>
+      <h2 className={styles.title}>{steps.title}</h2>
       <div className={styles.reportContains}>
         {steps.details?.map(step => (
           <ReportContain key={step.verb} {...step} iconSrc={steps.iconSrc} page={page} />
