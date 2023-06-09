d4dbc2d838dd23cc128244cefa59585cbbc37dd1
Rachel Skrenta <rachelskrenta@gmail.com>
2023-06-09 12:23:22 -0700

    fix: modified maxmind file paths

--- src/components/Test/Testing/TestingSection.js
+++ src/components/Test/Testing/TestingSection.js
@@ -6,7 +6,6 @@ import LoadBar from './LoadBar'
 import OoklaLoading from './OoklaLoading'
 import config from './testSrc/testConfig'
 import runTests from './testSrc/runTests'
-import { useNavigate } from 'react-router-dom'
 import styles from './TestingSection.module.css'
 import { useToolkitContext } from 'components/common/Context/ToolkitContext'
 import Survey from 'components/common/Survey/Survey'
@@ -18,7 +17,6 @@ const TestingSection = ({ setCurrentDisplay, setTestData }) => {
     surveySegment
   } = useToolkitContext()
 
-  const navigate = useNavigate()
   const [testSource, setTestSource] = useState('')
   const [testType, setTestType] = useState('')
   const [testProgress, setTestProgress] = useState('')
